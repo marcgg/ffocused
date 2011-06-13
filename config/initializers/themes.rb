b40970c7ba42986a9de406a3149f8c983cacb698
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-06-13 19:30:34 +0200

    rlvnt yellow theme

--- config/initializers/themes.rb
+++ config/initializers/themes.rb
@@ -3,5 +3,10 @@ THEMES = {
     :title => "Default Theme",
     :code => "default_css",
     :user => "<a href='http://twitter.com/marcgg'>@marcgg</a>"
+  },
+  2 => {
+    :title => "RLVNT Yellow",
+    :code => "rlvnt_yellow",
+    :user => "<a href='http://twitter.com/marcgg'>@marcgg</a>"
   }
 }
