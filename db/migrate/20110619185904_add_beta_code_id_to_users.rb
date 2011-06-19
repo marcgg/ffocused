886d1b673ba38c84aa7d457d8552381c8e359b07
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-06-19 21:20:51 +0200

    create first user beta code on the fly when needed

--- db/migrate/20110619185904_add_beta_code_id_to_users.rb
+++ db/migrate/20110619185904_add_beta_code_id_to_users.rb
@@ -1,6 +1,8 @@
 class AddBetaCodeIdToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :beta_code, :string
+    code = BetaCode.where(:code => "FirstUsers").first
+    BetaCode.create!(:code => "FirstUsers") unless code
     User.all.each do |user|
       user.update_attributes!(:beta_code => "FirstUsers")
     end
