ee624235a1d2302b4ab9c0f6a8fe73251b719b32
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-06-21 23:39:08 +0200

    open security breach to debug something

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
+require "openssl"
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Prtfl::Application.initialize!
+
+# Bypass some Facebook nonsense
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
