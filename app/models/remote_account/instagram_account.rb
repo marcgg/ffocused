e5fc1e36ccca450e8932433d3cacd017307b038f
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-08-27 15:24:54 +0200

    check instagram account expiration (issue #9)

--- app/models/remote_account/instagram_account.rb
+++ app/models/remote_account/instagram_account.rb
@@ -3,6 +3,14 @@ class RemoteAccount::InstagramAccount < RemoteAccount
     "instagram"
   end
 
+  def still_valid?
+    user = Fetchers::Instalight::User.new(access_token)
+    res = user.call_method("self")
+    return res["id"].to_s == self.remote_user_id.to_s
+  rescue Exception => e
+    return false
+  end
+
   def photos
     user = Fetchers::Instalight::User.new(access_token)
     user_data = user.call_method("#{remote_user_id}/media/recent", ["count=100"])
