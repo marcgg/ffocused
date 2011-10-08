31cf712c697ef8d7e8d0521c591ccb5f43708033
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-10-08 18:09:46 +0200

    Better signup path

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Prtfl::Application.routes.draw do
   resources :user_sessions
   match "/login"  => "user_sessions#new",     :as => :login
   match "/logout" => "user_sessions#destroy", :as => :logout
+  match "/signup" => "users#new"
 
   # HOME
   root :to => "home#index"
